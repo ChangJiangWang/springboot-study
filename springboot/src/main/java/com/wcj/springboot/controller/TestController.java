856b52159393f1a8a32745be843b9e0df87aca04
wcj021312 <2925428721@qq.com>
2018-04-09 22:42:24 +0800

    create

---
+++ springboot/src/main/java/com/wcj/springboot/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.wcj.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/helloworld")
+	public String getHelloWorld() {
+		return "hello word!!!";
+	}
+}
